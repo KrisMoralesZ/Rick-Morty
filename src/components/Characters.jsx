281bbbfc58c44f46f8bfe714527c25c37d70a1c5
Kritopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-12 14:09:10 -0700

    llamado api

---
+++ src/components/Characters.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+const Characters = () => {
+    const [characters, setCharacters] = useState([]);
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character/')
+        .then(Response => Response.json())
+        .then(data => setCharacters(data.results));
+    }, []);
+    return (
+        <div className="characters">
+            {characters.map(character =>(
+                <h2>{character.name}</h2>
+            ))
+            }
+        </div>
+    );
+}
+
+export default Characters;  
