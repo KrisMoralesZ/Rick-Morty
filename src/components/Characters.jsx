c590b30c8bd477e3907171b8adcaadf1ba8214f1
Kritopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-15 09:47:48 -0700

    useref for searcher

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState, useReducer, useMemo } from 'react'
+import React, { useEffect, useState, useReducer, useMemo, useRef } from 'react'
 import { Card } from 'react-bootstrap';
+
 const initialState = {
   favorites: []
 }
+
 const favoriteReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_TO_FAVORITE':
@@ -14,27 +16,34 @@ const favoriteReducer = (state, action) => {
       return state;
   }
 }
+
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
   const [search, setSearch] = useState('');
+  const searchInput = useRef(null);
+
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
       .then(Response => Response.json())
       .then(data => setCharacters(data.results));
   }, []);
+
   const handleClick = favorite => {
     dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
   }
-  const handleSearch = (event) => {
-    setSearch(event.target.value)
+
+  const handleSearch = () => {
+    setSearch(searchInput.current.value)
   }
+
   const filteredUsers = useMemo(() =>
     characters.filter((user) => {
       return user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
     }),
     [characters, search]
   )
+
   return (
     <div className="characters">
       {favorites.favorites.map(favorite => (
@@ -43,7 +52,7 @@ const Characters = () => {
         </li>
       ))}
       <div className="Search">
-        <input type="text" value={search} onChange={handleSearch} />
+        <input type="text" value={search} ref={searchInput} onChange={handleSearch} />
       </div>
       {
         filteredUsers.map(character => (
