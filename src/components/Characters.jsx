b3090347cc81c5feb5d58959e7e290e822b2d6b5
Kritopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-14 21:31:32 -0700

    searcher with memo

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from 'react'
+import React, { useEffect, useState, useReducer, useMemo } from 'react'
 import { Card } from 'react-bootstrap';
 const initialState = {
   favorites: []
@@ -17,6 +17,7 @@ const favoriteReducer = (state, action) => {
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+  const [search, setSearch] = useState('');
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
       .then(Response => Response.json())
@@ -25,6 +26,15 @@ const Characters = () => {
   const handleClick = favorite => {
     dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
   }
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+  const filteredUsers = useMemo(() =>
+    characters.filter((user) => {
+      return user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+    }),
+    [characters, search]
+  )
   return (
     <div className="characters">
       {favorites.favorites.map(favorite => (
@@ -32,15 +42,20 @@ const Characters = () => {
           {favorite.name}
         </li>
       ))}
-      {characters.map(character => (
-        <div className="item" key={character.id}>
-          <img src={character.image} alt="" />
-          <h2>{character.name}</h2>
-          <button type="button" onClick={() => handleClick(character)}>
-            Agregar a Favoritos
-          </button>
-        </div>
-      ))}
+      <div className="Search">
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
+      {
+        filteredUsers.map(character => (
+          <div className="item" key={character.id}>
+            <img src={character.image} alt="" />
+            <h2>{character.name}</h2>
+            <button type="button" onClick={() => handleClick(character)}>
+              Agregar a Favoritos
+            </button>
+          </div>
+        ))
+      }
     </div>
   );
 }
